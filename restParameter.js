19318d0d22d8b1e7e4d66f362fd0310db754d88e
Angekarara <mbabange2020@gmail.com>
2024-02-29 15:06:37 +0200

    restParameter ex2

--- restParameter.js
+++ restParameter.js
@@ -9,3 +9,21 @@ function print(...items) {
   }
 }
 print(["ar", "rea", "jao", "ant", "nana", "opa"]);
+
+//Example 2
+
+function average(...nums) {
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  if (nums.length === 0) {
+    return 0;
+  }
+  return sum / nums.length;
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
