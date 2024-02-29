82bbf2c9e9ef93209e632768064c21482997ebec
Angekarara <mbabange2020@gmail.com>
2024-02-29 14:43:54 +0200

    restParameter ex

--- restParameter.js
+++ restParameter.js
@@ -2,3 +2,10 @@ const array = [1, 2, 3, 4, 5, 6];
 const [x, y, z, ...h] = array;
 console.log(x, y, z, h);
 console.log(array);
+
+function print(...items) {
+  for (let item of items) {
+    console.log(item);
+  }
+}
+print(["ar", "rea", "jao", "ant", "nana", "opa"]);
