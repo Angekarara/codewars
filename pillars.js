c0fb25f1215b46954e91a39e370f2a161ddce643
Angekarara <mbabange2020@gmail.com>
2024-03-20 23:25:40 +0200

    new changes on pillars kata

--- pillars.js
+++ pillars.js
@@ -1,5 +1,4 @@
 function pillars(numPill, dist, width) {
-  // your code here
   console.log(numPill, dist, width);
   return numPill == 1 ? 0 : (numPill - 2) * width + 100 * dist * (numPill - 1);
 }
