bb59e2e73efdc8bd3b90e37a60a85f3bdbdf7387
Angekarara <mbabange2020@gmail.com>
2024-06-18 11:50:44 +0200

    Keep up the hoop

--- hoop.js
+++ hoop.js
@@ -1,5 +1,9 @@
-function hoopCount (n) {
-    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
-  }
-  
-  
+function hoopCount(n) {
+  return n >= 10
+    ? "Great, now move on to tricks"
+    : "Keep at it until you get it";
+}
+
+console.log(hoopCount(19));
+
+console.log(hoopCount(9));
