002619d9c953c095dead14a04dd07be609a03102
Angekarara <mbabange2020@gmail.com>
2024-06-18 11:40:38 +0200

    Calculate Parity bit!

---
+++ parityBit.js
@@ -0,0 +1,14 @@
+function checkParity(parity, bin){
+    let results;
+    const arr = bin.toString().split("").map(num => Number(num)).filter(n => n === 1)
+    if(parity === 'even' && arr.length % 2 === 0){
+      results = 0;
+    }
+    else if(parity === 'odd' && arr.length % 2 !== 0){
+      results = 0;
+    }
+    else{
+     results = 1
+    }
+    return results 
+  }
