6fc481e451d36fbc88e7f2f8d42d75b1cc8d011f
Angekarara <mbabange2020@gmail.com>
2024-07-22 10:38:51 +0200

    Printer Errors  kata

--- printer-errors.js
+++ printer-errors.js
@@ -1,4 +1,6 @@
 function printerError(s) {
-    const errors = s.match(/[^a-m]/gi)
-   return errors ? `${errors.length}/${s.length}` : `0/${s.length}`
+  const errors = s.match(/[^a-m]/gi);
+  return errors ? `${errors.length}/${s.length}` : `0/${s.length}`;
 }
+console.log(printerError("aaabbbbhaijjjm"));
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"));
