9e997bf38e8e465b45342eeb02ab660695d2ad5f
Angekarara <mbabange2020@gmail.com>
2024-07-28 22:12:18 +0200

    Find the divisors! kata

--- find-divisor.js
+++ find-divisor.js
@@ -1,12 +1,14 @@
 function divisors(integer) {
-    let arr = []
-      if(integer > 1){
-        for(let i=2; i<integer; i++){
-          if(integer % i === 0){
-             arr.push(i)
-          }
-          
+  let arr = [];
+  if (integer > 1) {
+    for (let i = 2; i < integer; i++) {
+      if (integer % i === 0) {
+        arr.push(i);
       }
     }
-    return arr.length ? arr : `${integer} is prime`;
   }
+  return arr.length ? arr : `${integer} is prime`;
+}
+console.log(divisors(15));
+console.log(divisors(13));
+console.log(divisors(12));
