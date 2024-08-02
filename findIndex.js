7cd544e4c111efa9eb522f94dbb5764a0e920a97
Angekarara <mbabange2020@gmail.com>
2024-08-02 23:16:48 +0200

    Be Concise IV - Index of an element in an array kata

--- findIndex.js
+++ findIndex.js
@@ -1,1 +1,6 @@
 const find = (a, e) => (a.includes(e) ? a.indexOf(e) : "Not found");
+let array = [3,5,9,1,2,8]
+console.log(find(array, 3))
+console.log(find(array, 1))
+console.log(find(array, 4))
+console.log(find(array, 5))
