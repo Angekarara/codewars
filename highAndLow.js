9de8fe3421a77d6d4eae8d514e76df5686eaeb3e
Angekarara <mbabange2020@gmail.com>
2024-08-03 19:43:24 +0200

    Highest and Lowest kata

--- highAndLow.js
+++ highAndLow.js
@@ -1,6 +1,8 @@
-function highAndLow(numbers){
-    const arr = numbers.split(" ")
-    const max = Math.max(...arr)
-    const min = Math.min(...arr)
-    return `${max} ${min}`
-  }
+function highAndLow(numbers) {
+  const arr = numbers.split(" ");
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  return `${max} ${min}`;
+}
+console.log(highAndLow("2 96 34 8 10"));
+console.log(highAndLow("9 3 8 100"));
