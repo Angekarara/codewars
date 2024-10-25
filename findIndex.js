f7eaa576a01a5265e5cca93bdb5790ab2f9d430a
Angekarara <mbabange2020@gmail.com>
2024-10-25 19:28:59 +0200

    removed console logs

--- findIndex.js
+++ findIndex.js
@@ -1,6 +1,2 @@
 const find = (a, e) => (a.includes(e) ? a.indexOf(e) : "Not found");
 let array = [3,5,9,1,2,8]
-console.log(find(array, 3))
-console.log(find(array, 1))
-console.log(find(array, 4))
-console.log(find(array, 5))
