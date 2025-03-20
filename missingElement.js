46f7d10cc003a6fafb679fb39510eff1f733b7ca
Ange Karara Mbabazi <99403587+Angekarara@users.noreply.github.com>
2025-03-20 19:20:10 +0200

    Update missingElement.js

--- missingElement.js
+++ missingElement.js
@@ -5,5 +5,3 @@ function getMissingElement(superImportantArray) {
     }
   }
 }
-console.log(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4]));
-console.log(getMissingElement([9, 2, 4, 5, 7, 0, 8, 6, 1]));
