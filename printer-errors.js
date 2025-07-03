e6fb4b777eeac6f515ddb22a2055c96a7a75fd9b
Ange Karara Mbabazi <99403587+Angekarara@users.noreply.github.com>
2025-07-03 23:21:31 +0200

    Update printer-errors.js

--- printer-errors.js
+++ printer-errors.js
@@ -2,5 +2,3 @@ function printerError(s) {
   const errors = s.match(/[^a-m]/gi);
   return errors ? `${errors.length}/${s.length}` : `0/${s.length}`;
 }
-console.log(printerError("aaabbbbhaijjjm"));
-console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"));
