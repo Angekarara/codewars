7e226f6b0916158419f210976f418365e4f2460d
Ange Karara Mbabazi <99403587+Angekarara@users.noreply.github.com>
2025-07-05 21:18:00 +0200

    Update tailSwap.js

--- tailSwap.js
+++ tailSwap.js
@@ -1,5 +1,4 @@
 function tailSwap(arr) {
     const [ar1, ar2] = arr.map(e => e.split(':'))
-    return [`${ar1[0]}:${ar2[1]}`,`${ar2[0]}:${ar1[1]}`] 
-     
+    return [`${ar1[0]}:${ar2[1]}`,`${ar2[0]}:${ar1[1]}`]
    }
