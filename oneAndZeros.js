c8234b34d65288eebfcc820627f7a3a4af8177cd
Ange Karara Mbabazi <99403587+Angekarara@users.noreply.github.com>
2025-07-06 21:51:50 +0200

    Update oneAndZeros.js

--- oneAndZeros.js
+++ oneAndZeros.js
@@ -3,6 +3,3 @@ const binaryArrayToNumber = arr => {
     console.log(str)
     return parseInt(str,2)
   };
-  console.log(binaryArrayToNumber([0, 0, 0, 1]))
-  console.log(binaryArrayToNumber([0, 0, 1, 0]))
-  console.log(binaryArrayToNumber([1, 1, 1, 1]))
