ca77e2de6dae6b51491de32fe38a157d70577037
Ange Karara Mbabazi <99403587+Angekarara@users.noreply.github.com>
2025-07-13 22:14:25 +0200

    Update isletter.js

--- isletter.js
+++ isletter.js
@@ -1,7 +1,3 @@
-// function isItLetter(character) {
-//   const letters = character.match(/[a-z]/gi);
-//   return letters ? true : false;
-// }
 function isItLetter(character) {
   return character.match(/[a-z]/i) ? true : false;
 }
