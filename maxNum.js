01a3d6b1d9dcdfc77bc047edc4fbe798f6fc4144
Ange Karara Mbabazi <99403587+Angekarara@users.noreply.github.com>
2025-07-24 22:21:45 +0200

    Update maxNum.js

--- maxNum.js
+++ maxNum.js
@@ -1,5 +1,3 @@
 function findMaxNumber(numbers) {
   return numbers.reduce((max, num) => (num > max ? num : max));
 }
-console.log(findMaxNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-console.log(findMaxNumber([1, 2, 3, 4, 5, 10, 9, 8, 7, 6]));
