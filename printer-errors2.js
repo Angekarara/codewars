da6809556bd600e42253abdf9a362f71259ab3d8
Ange Karara Mbabazi <99403587+Angekarara@users.noreply.github.com>
2025-08-01 20:03:26 +0200

    Update printer-errors2.js

--- printer-errors2.js
+++ printer-errors2.js
@@ -2,7 +2,6 @@ function printerErrors2(s) {
   const arr = ["x", "y", "z"];
   let count = 0;
   const stringArray = s.split("");
-  console.log(stringArray);
   for (let i = 0; i <= arr.length; i++) {
     for (j = 0; j < i; j++) {
       if (s[i] !== arr[i]) {
